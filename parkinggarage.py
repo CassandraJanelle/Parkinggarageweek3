5a502e2b7861c8e4ab0b1b74fe27c9ec0a00d907
pateltarun1211 <pateltarun1211@gmail.com>
2022-10-30 15:34:33 -0500

    sunday afternoon update

--- parkinggarage.py
+++ parkinggarage.py
@@ -1,15 +1,45 @@
 class Garage:
     def __init__(self):
         self.tickets = []
-        self.parkingSpaces = []
-        self.currentTicket = {}
+        self.parkingspaces = []
+        self.curentTicket = {}
 
     def takeTicket(self):
-        if self.tickets == []:
-            print("Parking lot is full")
-        else:
-            for ticket in self.tickets:
-                self.tickets.pop(ticket)
+        pass
+
+
+    def payForParking(self):
+        payment = 0
+        while payment <=0:
+            payment = input("Please enter payment amount: ")
+            if payment <= 0:
+                print("Invalid amount")
+        
+        self.curentTicket['paid'] = True
+    
+    def leaveGarage(self):
+        pass
 
     def run(self):
-        
+        print("""
+        What would you like to do?
+        1 - Take Ticket
+        2 - Pay for Parking
+        3 - Leave Garage
+        4 - Quit
+        """)
+
+        while True:
+            response = input("Choose a numbered option from list: ")
+
+            if response == '1':
+                self.takeTicket()
+            elif response == '2':
+                self.payForParking
+            elif response == '3':
+                self.leaveGarage()
+            elif response == '4':
+                print("Thanks for coming to 'Parking up the wrong tree', where if you don't pay there will be tyre consequences!")
+                break
+            else:
+                print("Invalid response, please choose numbered option from list!")
